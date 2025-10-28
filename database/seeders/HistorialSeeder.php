49c9b3f3d36502baf23cb625378f04dc3af5b502
Benjamin <programador.ben@gmail.com>
2025-10-27 23:47:34 -0400

    feat: :seedling: seeders para historial

--- database/seeders/HistorialSeeder.php
+++ database/seeders/HistorialSeeder.php
@@ -17,12 +17,120 @@ class HistorialSeeder extends Seeder
             ['grupo_id' => 8, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
             ['grupo_id' => 10, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
 
-            //2do Semestre
-            ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
-            ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
-            ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
-            ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
-            ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+            // //2do Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //3er Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //4to Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //5to Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+
+
+            
+            
+        ];
+
+        $gruposWilder = [
+            //1er Semestre
+            ['grupo_id' => 1, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            ['grupo_id' => 4, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            ['grupo_id' => 6, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            ['grupo_id' => 8, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            ['grupo_id' => 10, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //2do Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //3er Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //4to Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //5to Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+
+
+            
+            
+        ];
+
+        $gruposTifanny = [
+            //1er Semestre
+            ['grupo_id' => 1, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            ['grupo_id' => 4, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            ['grupo_id' => 6, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            ['grupo_id' => 8, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            ['grupo_id' => 10, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
+
+            // //2do Semestre
+            // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
+            // ['grupo_id' => 14, 'estudiante_id' => 1, 'nota' => 90, 'creditos' => '5'],
+            // ['grupo_id' => 16, 'estudiante_id' => 1, 'nota' => 78, 'creditos' => '5'],
+            // ['grupo_id' => 18, 'estudiante_id' => 1, 'nota' => 88, 'creditos' => '5'],
+            // ['grupo_id' => 20, 'estudiante_id' => 1, 'nota' => 92, 'creditos' => '5'],
 
             // //3er Semestre
             // ['grupo_id' => 12, 'estudiante_id' => 1, 'nota' => 85, 'creditos' => '5'],
@@ -64,6 +172,14 @@ class HistorialSeeder extends Seeder
         ];
         
         foreach ($gruposBenjamin as $registro){
+            GrupoEstudiante::create($registro);
+        }
+
+        foreach ($gruposWilder as $registro){
+            GrupoEstudiante::create($registro);
+        }
+
+        foreach ($gruposTifanny as $registro){
             GrupoEstudiante::create($registro);
         }
     }
